a2598e89034a010557eea6ae677eb6003bf14c4f
Diogo Araújo <93762486+diogoaraujo017@users.noreply.github.com>
2023-11-10 01:36:44 +0000

    Update FS_Tracker.java

--- src/FS_Tracker.java
+++ src/FS_Tracker.java
@@ -3,99 +3,96 @@ import java.io.*;
 import java.net.*;
 
 public class FS_Tracker {
-    private static String ip_address = "10.0.1.10";
-    private static int tcp_port = 42069;
-    private int connected_nodes;
-    private Map<String, List<String>> nodes;  // ip do nodo e lista com o nome dos ficheiros do nodo
+    
+    // Server network data
+    private static String ip = "10.0.1.10";
+    private static int port = 42069;
 
+    // Node derived data
+    private int numConnectedNodes;
+    private Map<String, List<String>> nodesFiles;   // Key: Node Ip
+                                                    // Value : Node files 
+    
     // Constructors
 
     public FS_Tracker() {
-        this.connected_nodes = 0;
-        this.nodes = new HashMap<String, List<String>>();
+        this.numConnectedNodes = 0;
+        this.nodesFiles = new HashMap <String,List<String>> ();
     }
 
     // Getters
 
-    public String get_tracker_ip_address() {
-        return this.ip_address;
+    public String getIp() {
+        return FS_Tracker.ip;
     }
 
-    public int get_tracker_tcp_port() {
-        return this.tcp_port;
+    public int getPort() {
+        return FS_Tracker.port;
     }
 
-    public int get_connected_nodes() {
-        return this.connected_nodes;
+    public int getNumNodesConnected() {
+        return this.numConnectedNodes;
     }
 
-    public Map<String, List<String>> get_tracker_nodes() {
-        return this.nodes;
-    }
-
-    // Setters
-
-    public void set_tracker_ip_address(String ip_address) {
-        this.ip_address = ip_address;
-    }
-
-    public void set_tracker_tcp_port(int tcp_port) {
-        this.tcp_port = tcp_port;
+    public Map<String, List<String>> getNodesFiles() {
+        return this.nodesFiles;
     }
 
     // Methods
 
-    public String search_file(String file_name) {
-        String file_node = null;
-
-        for (Map.Entry<String, List<String>> entry : this.nodes.entrySet()) {
-            String key = entry.getKey();
-            List<String> node_files = entry.getValue();
-
-            if (node_files.contains(file_name)) {
-                file_node = key;
-            }
-        }
-        return file_node;
-    }
-
-    public void print_connected_nodes() {
+    public void printConnectedNodes() {
         System.out.println("Connected nodes:");
 
-        if (get_tracker_nodes().entrySet().isEmpty()) {
+        if (getNumNodesConnected()==0) {
             System.out.println("Zero connected nodes\n");
-        }
-
-        else {
-            for (Map.Entry<String, List<String>> entry : get_tracker_nodes().entrySet()) {
-                System.out.println(entry.getKey());
+        
+        } else {
+            System.out.println("Os nodos conectados sao: \n");
+            for (Map.Entry<String, List<String>> entry : getNodesFiles().entrySet()) {
+                System.out.println("  ->" + entry.getKey() + "\n");
             }
             System.out.println("\n");
         }
     }
 
-    public void delete_node(String node_ip_address) {
-        this.nodes.remove(node_ip_address);
-        this.connected_nodes--;
+    public void removeNodeData(String nodeIp) {
+        this.nodesFiles.remove(nodeIp);
+        this.numConnectedNodes--;
     }
 
-    public void registerNodeData(String node_ip_address, List<String> files) {
-        List<String> node_files = new ArrayList<String>();
-        node_files = files;
-        this.nodes.put(node_ip_address, node_files);
+    public void insertNewNode(String nodeIp, List<String> files) {
+        this.nodesFiles.put(nodeIp, files);
+        this.numConnectedNodes++;
     }
 
+    public void insertNodeData(String nodeIp, String file) {
+        this.nodesFiles.get(nodeIp).add(file);
+    }
+
+    public List<String> getFileLocations(String fileName) {
+        List<String> nodeIps = new ArrayList<String> ();
+
+        for (Map.Entry<String, List<String>> entry : this.nodesFiles.entrySet()) {
+            String key = entry.getKey();
+            List<String> nodeFiles = entry.getValue();
+
+            if (nodeFiles.contains(fileName)) {
+                nodeIps.add(key);
+            }
+        }
+        return nodeIps;
+
+    }
 
     public static void main(String[] args) {
-        FS_Tracker tracker = new FS_Tracker();
+        FS_Tracker serverData = new FS_Tracker();
 
         try {
             
-            ServerSocket server = new ServerSocket(tracker.get_tracker_tcp_port());
-            System.out.println("\u001B[32mServidor ativo em " +  ip_address + " porta " +  tcp_port +" \u001B[0m \n");
+            ServerSocket serverSocket = new ServerSocket(serverData.getPort());
+            System.out.println("\u001B[32m Servidor ativo com ip " +  serverData.getIp() + " e com porta " +  serverData.getPort() +" \u001B[0m \n");
 
-            
-            Thread commandsTracker = new Thread ( () -> {
+            Thread terminalTracker = new Thread ( () -> {
 
                 while (true) { 
                        
@@ -103,17 +100,17 @@ public class FS_Tracker {
                         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                         String input = reader.readLine();
                     
-                        String[] command_parts = input.split(" ");
-                        String command_name = command_parts[0];
-                        List<String> commandArguments = Arrays.asList(Arrays.copyOfRange(command_parts, 1, command_parts.length));
+                        String[] commandParts = input.split(" ");
+                        String commandName = commandParts[0];
+                        List<String> commandArguments = Arrays.asList(Arrays.copyOfRange(commandParts, 1, commandParts.length));
         
-                        switch (command_name) {
+                        switch (commandName) {
                             case "connections":
-                                tracker.print_connected_nodes();
+                                serverData.printConnectedNodes();;
                                 break;
                         
                             case "exit":
-                                server.close();
+                                serverSocket.close();
                                 System.out.println("Tracker is exiting.");
                                 System.exit(0);
                                 break;
@@ -129,19 +126,13 @@ public class FS_Tracker {
                 } 
               }
             );
-
-            commandsTracker.start();
+            terminalTracker.start();
                 
 
             while (true) {
-                try {
-                    Socket node_socket = server.accept();
-                    Thread t = new Thread(new ConnectionsHandlerTracker(node_socket,tracker));
-                    t.start();
-                
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                Socket nodeSocket = serverSocket.accept();
+                Thread t = new Thread(new ConnectionsHandlerTracker(nodeSocket,serverData));
+                t.start();
             }
             
 
